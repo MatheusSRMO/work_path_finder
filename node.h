6bbcdbed2ef24e1276b1099228cb4d550e9c0d49
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2023-05-31 01:57:25 -0300

    feat: finish node struct

--- node.h
+++ node.h
@@ -16,12 +16,25 @@ typedef enum {
 
 typedef struct node* NodePtr;
 
+
+/// @brief Node constructor
+/// @param data void pointer to data
+/// @param type enum DataType
+/// @return new NodePtr
 NodePtr node_create(void* data, DataType type);
 
-void node_set_prev(NodePtr node, NodePtr prev);
-
+/// @brief Get previous node
+/// @param node NodePtr
+/// @return prev NodePtr
 NodePtr node_get_prev(NodePtr node);
 
+/// @brief Set previous node
+/// @param node NodePtr
+/// @param prev NodePtr
+void node_set_prev(NodePtr node, NodePtr prev);
+
+/// @brief Node destructor
+/// @param node NodePtr
 void node_destroy(NodePtr node);
 
 #endif // NODE_H
